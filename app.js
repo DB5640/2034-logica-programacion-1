88eab73d2c6d461e10636f4a4fde75b82aaf6a7c
David B <dbolano@paco.com.co>
2024-01-18 08:25:12 -0500

    Inicia la app

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let numeroSecreto = "Si";
+let numeroUsuario = prompt("Dime si me amas (Si o No):")
+console.log(" "+numeroUsuario);
+
+if(numeroUsuario == numeroSecreto){
+    alert ("Me parece muy bien mi amorcito");
+    
+}
+else {"Como que no!!😭😭"};
